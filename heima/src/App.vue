00358b13872b4c8d88b3cae1bb3db07bad04d090
zty1999 <1685470954@qq.com>
2019-08-16 11:38:08 +0800

    heima

---
+++ heima/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <mt-header fixed title="黑马项目"></mt-header>
+    </div>
+
+    <router-view/>
+
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+    
+  </div>
+</template>
+
+<style>
+#app {
+  padding-top: 40px;
+  padding-bottom: 40px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ heima/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+// Vue.use(MintUI)
+// import { Toast } from 'mint-ui';
+
+import { Header } from 'mint-ui';
+
+Vue.component(Header.name, Header);
+
+
+import $ from 'jquery' ;
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+import './lib/mui/css/mui.min.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
