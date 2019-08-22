8029a4168bc05bda584762f5c8ea1ec0e8928052
zty1999 <1685470954@qq.com>
2019-08-22 11:30:23 +0800

    之后开始学习vuex

--- heima/src/components/buy/BuyInfo.vue
+++ heima/src/components/buy/BuyInfo.vue
@@ -21,10 +21,17 @@
 			<div class="mui-card-content">
 				<div class="mui-card-content-inner">
                     <p>市场价：<del>￥{{buyinfo.market_price}}</del>&nbsp;&nbsp;销售价：<span class='now_price'>￥{{buyinfo.sell_price}}</span></p>
-                    <p>购买数量：<BuyInfo_NumberBox></BuyInfo_NumberBox></p>
+                    <p>购买数量：<BuyInfo_NumberBox @getcount="getSelectedCount"
+                    :max='buyinfo.stock_quantity'></BuyInfo_NumberBox></p>
                     <p>
                         <mt-button type='primary' size='small'>立即购买</mt-button>
-                        <mt-button type='danger' size='small' @click="addToShopCar">加入购物车</mt-button>
+                        <mt-button type='danger' size='small' @click="addToShopCar()">加入购物车</mt-button>
+                             <!-- 分析： 如何实现加入购物车时候，拿到 选择的数量 -->
+            <!-- 1. 经过分析发现： 按钮属于 goodsinfo 页面， 数字 属于 numberbox 组件 -->
+            <!-- 2. 由于涉及到了父子组件的嵌套了，所以，无法直接在 goodsinfo 页面zhong 中获取到 选中的商品数量值-->
+            <!-- 3. 怎么解决这个问题：涉及到了 子组件向父组件传值了（事件调用机制） -->
+            <!-- 4. 事件调用的本质： 父向子传递方法，子调用这个方法， 同时把 数据当作参数 传递给这个方法 -->
+                    
                     </p>
                 </div>
 			</div>
@@ -56,7 +63,8 @@ export default {
             id: this.$route.params.id,//从路由中获取到的图片id
             slideShowList: [],
             buyinfo: {},
-            ballFlag: false,//控制小球隐藏和显示的控制符
+            ballFlag: false,//控制小球隐藏和显示的标识符
+            selectedCount: 1//保存用户选中的商品数量，默认为1
         }
     },
     created() {
@@ -101,11 +109,11 @@ export default {
              //添加到购物车 点击按钮小球出现
             this.ballFlag = !this.ballFlag
         },
-        beforeEnter(el) {
-      el.style.transform = "translate(0, 0)";
-    },
-    enter(el, done) {
-      el.offsetWidth;
+        beforeEnter(el) {//从小球初始位置开始
+            el.style.transform = "translate(0, 0)";
+        },
+        enter(el, done) {
+            el.offsetWidth;
 
       // 小球动画优化思路：
       // 1. 先分析导致 动画 不准确的 本质原因： 我们把 小球 最终 位移到的 位置，已经局限在了某一分辨率下的 滚动条未滚动的情况下；
@@ -115,23 +123,27 @@ export default {
       // 5. 如何 获取 徽标和小球的 位置？？？   domObject.getBoundingClientRect()
 
       // 获取小球的 在页面中的位置
-    //   const ballPosition = this.$refs.ball.getBoundingClientRect();
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
       // 获取 徽标 在页面中的位置
-    //   const badgePosition = document
-    //     .getElementById("badge")
-    //     .getBoundingClientRect();
+      const badgePosition = document
+        .getElementById("badge")//需现在App组件上给徽标一个id="badge"
+        .getBoundingClientRect();
 
-    //   const xDist = badgePosition.left - ballPosition.left;
-    //   const yDist = badgePosition.top - ballPosition.top;
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
 
-      el.style.transform = "translate(93px, 230px)";
-      el.style.transition = "all 2s cubic-bezier(.4,-0.3,1,.68)";
-    //   done();
-    },
-    afterEnter(el) {
-      this.ballFlag = !this.ballFlag;
-    },
-
+            el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+            el.style.transition = "all .5s cubic-bezier(.4,-0.3,1,.68)";
+            done();
+        },
+        afterEnter(el) {//动画结束后小球隐藏
+            this.ballFlag = !this.ballFlag;
+        },
+        getSelectedCount(count) {
+        // 当子组件把 选中的数量传递给父组件的时候，把选中的值保存到 data 上
+            this.selectedCount = count;
+            console.log("父组件拿到的数量值为： " + this.selectedCount);
+        }
     },
     components: {
         swiper,
@@ -161,8 +173,8 @@ export default {
         }
     }
     .ball {
-        width: 20px;
-        height: 20px;
+        width: 15px;
+        height: 15px;
         border-radius: 50%;
         background-color: red;
         position: absolute;
--- heima/src/components/subcomponents/BuyInfo_NumberBox.vue
+++ heima/src/components/subcomponents/BuyInfo_NumberBox.vue
@@ -1,8 +1,8 @@
 
 <template>
-    <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+    <div class="mui-numbox" data-numbox-min='1'>
     	<button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-	    <input id="test" class="mui-input-numbox" type="number" value="5" />
+	    <input id="test" class="mui-input-numbox" type="number" value="1" @change="countChanged" ref='numbox'/>
 	    <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
     </div>
 </template>
@@ -11,9 +11,27 @@ import mui from '../../lib/mui/js/mui.min.js'//注意引入的要是min.js文件
 export default {
     mounted() {
         mui('.mui-numbox').numbox()//初始化数字选择框组件，官网文档可找到初始化数字输入框代码mui('').numbox()，添加进此组件类名，初始化此组件
+    },
+    methods: {
+        countChanged() {
+            // 每当 文本框的数据被修改的时候，立即把 最新的数据，通过事件调用，传递给父组件
+            console.log(this.$refs.numbox.value);
+            this.$emit("getcount", parseInt(this.$refs.numbox.value));
+        }
+    },
+    props: ['max'],
+    watch: {
+         // 属性监听
+        max: function(newVal, oldVal) {
+            // 使用 JS API 设置 numbox 的最大值
+            mui(".mui-numbox")
+            .numbox()
+            .setOption("max", newVal);
+        }
     }
 }
 </script>
 <style lang="scss" scoped>
+
 
 </style>
